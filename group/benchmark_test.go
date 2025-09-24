776ac78ff7a15a0d8454efd7c38e234c7eabd716
Tahsin716 <tahsin.rashad716@gmail.com>
2025-09-25 00:52:25 +0600

    basic execution benchmark added

--- group/benchmark_test.go
+++ group/benchmark_test.go
@@ -1,6 +1,7 @@
 package group
 
 import (
+	"context"
 	"testing"
 )
 
@@ -8,5 +9,26 @@ func BenchmarkGroupCreation(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		g := New()
 		_ = g
+	}
+}
+
+func BenchmarkGroupCreationWithOptions(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		g := New(WithErrorMode(FailFast))
+		_ = g
+	}
+}
+
+func BenchmarkBasicExecution(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		g := New()
+
+		for j := 0; j < 10; j++ {
+			g.Go(func(ctx context.Context) error {
+				return nil
+			})
+		}
+
+		g.Wait()
 	}
 }
