25a2ea5ccf7dc249e5ea7932ad2ac05166a66fd4
Tahsin716 <tahsin.rashad716@gmail.com>
2025-09-24 23:58:25 +0600

    TestNewWithTimeout added

--- group/group_test.go
+++ group/group_test.go
@@ -3,6 +3,7 @@ package group
 import (
 	"context"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -37,5 +38,25 @@ func TestNewWithContext(t *testing.T) {
 	g2 := NewWithContext(context.TODO())
 	if g2 == nil {
 		t.Fatal("NewWithContext(context.TODO()) returned nil")
+	}
+}
+
+func TestNewWithTimeout(t *testing.T) {
+	timeout := 100 * time.Millisecond
+	g := NewWithTimeout(timeout)
+
+	deadline, ok := g.ctx.Deadline()
+	if !ok {
+		t.Error("Context should have deadline")
+	}
+
+	if time.Until(deadline) > timeout+10*time.Millisecond {
+		t.Error("Deadline is too far in the future")
+	}
+
+	// Test with invalid timeout
+	g2 := NewWithTimeout(-1 * time.Second)
+	if g2 == nil {
+		t.Fatal("NewWithTimeout with negative value should still create group")
 	}
 }
