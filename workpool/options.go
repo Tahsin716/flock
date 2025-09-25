983ca2ad4aecce16bb2cb572ddf7012a29b9d570
Tahsin716 <tahsin.rashad716@gmail.com>
2025-09-25 19:50:44 +0600

    options added for setting pool config

--- workpool/options.go
+++ workpool/options.go
@@ -5,6 +5,9 @@ import (
 	"time"
 )
 
+// Option configures a Group
+type Option func(*Config)
+
 // Config holds pool configuration options
 type Config struct {
 	MaxWorkers      int
@@ -20,5 +23,45 @@ func DefaultConfig() Config {
 		ResultBuffer:    100,
 		MaxIdleTime:     10 * time.Second,
 		CleanupInterval: 30 * time.Second,
+	}
+}
+
+// WithMaxWorkers sets the maximum workers
+func WithMaxWorkers(worker int) Option {
+	return func(c *Config) {
+		if worker <= 0 {
+			worker = runtime.GOMAXPROCS(0) * 2
+		}
+		c.MaxWorkers = worker
+	}
+}
+
+// WithResultBuffer sets the total result buffer
+func WithResultBuffer(buffer int) Option {
+	return func(c *Config) {
+		if buffer <= 0 {
+			buffer = 100
+		}
+		c.ResultBuffer = buffer
+	}
+}
+
+// WithMaxIdleTime sets the maximum idle time for a goroutine
+func WithMaxIdleTime(t time.Duration) Option {
+	return func(c *Config) {
+		if t <= 0 {
+			t = 10 * time.Second
+		}
+		c.MaxIdleTime = t
+	}
+}
+
+// WithCleanupInterval sets the time interval purge idle goroutines
+func WithCleanupInterval(t time.Duration) Option {
+	return func(c *Config) {
+		if t <= 0 {
+			t = 10 * time.Second
+		}
+		c.CleanupInterval = t
 	}
 }
--- workpool/pool.go
+++ workpool/pool.go
@@ -3,6 +3,7 @@ package workpool
 import (
 	"context"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -50,4 +51,89 @@ type Pool[T any] struct {
 	// Configuration
 	maxIdleTime     time.Duration
 	cleanupInterval time.Duration
+}
+
+// New creates a new high-performance worker pool
+func New[T any](opts ...Option) *Pool[T] {
+	config := DefaultConfig()
+
+	for _, opt := range opts {
+		opt(&config)
+	}
+
+	p := &Pool[T]{
+		workers:         make([]*worker[T], 0, config.MaxWorkers),
+		workerCh:        make(chan *worker[T], config.MaxWorkers),
+		capacity:        int32(config.MaxWorkers),
+		results:         make(chan Result[T], config.ResultBuffer),
+		stopCh:          make(chan struct{}),
+		maxIdleTime:     config.MaxIdleTime,
+		cleanupInterval: config.CleanupInterval,
+	}
+
+	// Initialize result pool
+	p.resultPool.New = func() interface{} {
+		return &Result[T]{}
+	}
+
+	// Pre-allocate all workers to avoid allocation overhead
+	for i := 0; i < config.MaxWorkers; i++ {
+		w := &worker[T]{
+			jobCh: make(chan Job[T], 1),
+			pool:  p,
+		}
+		w.lastUsed = time.Now().UnixNano()
+		p.workers = append(p.workers, w)
+		p.workerCh <- w // All workers start available
+	}
+
+	// Start cleanup goroutine for idle workers
+	if config.CleanupInterval > 0 {
+		p.wg.Add(1)
+		go p.cleanupLoop()
+	}
+
+	return p
+}
+
+// cleanupLoop removes idle workers periodically (ants-style feature)
+func (p *Pool[T]) cleanupLoop() {
+	defer p.wg.Done()
+
+	ticker := time.NewTicker(p.cleanupInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			p.cleanupIdleWorkers()
+		case <-p.stopCh:
+			return
+		}
+	}
+}
+
+// cleanupIdleWorkers removes workers that have been idle too long
+func (p *Pool[T]) cleanupIdleWorkers() {
+	now := time.Now().UnixNano()
+	maxIdleNanos := p.maxIdleTime.Nanoseconds()
+
+	// Check idle workers and remove old ones
+	availableWorkers := len(p.workerCh)
+	for i := 0; i < availableWorkers; i++ {
+		select {
+		case w := <-p.workerCh:
+			lastUsed := atomic.LoadInt64(&w.lastUsed)
+			if now-lastUsed > maxIdleNanos {
+				// Worker is too old, don't return it to pool
+				// It will exit when it tries to return
+				continue
+			}
+			// Worker is still fresh, return to pool
+			p.workerCh <- w
+		default:
+			// No more workers to check
+			return
+		}
+	}
 }
