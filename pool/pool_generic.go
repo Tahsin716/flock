fe2fddcd3960bd1f96197c79f626b2f440b0b28b
Tahsin716 <tahsin.rashad716@gmail.com>
2025-09-30 20:19:00 +0600

    pool generic initialized

---
+++ pool/pool_generic.go
@@ -0,0 +1,24 @@
+package pool
+
+// Generic pool with function
+type PoolWithFuncGeneric[T any] struct {
+	pool *Pool
+	fn   func(T)
+}
+
+// NewPoolWithFuncGeneric creates a generic pool
+func NewPoolWithFuncGeneric[T any](size int, fn func(T), opts ...Option) (*PoolWithFuncGeneric[T], error) {
+	if fn == nil {
+		return nil, ErrFuncNil
+	}
+
+	pool, err := NewPool(size, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PoolWithFuncGeneric[T]{
+		pool: pool,
+		fn:   fn,
+	}, nil
+}
