024254da8ebeb6f5cb4e65bbc21d1ff611cbdf53
Tahsin716 <tahsin.rashad716@gmail.com>
2025-09-30 20:20:24 +0600

    wrapped method for pool added

--- pool/pool_generic.go
+++ pool/pool_generic.go
@@ -1,5 +1,7 @@
 package pool
 
+import "time"
+
 // Generic pool with function
 type PoolWithFuncGeneric[T any] struct {
 	pool *Pool
@@ -29,3 +31,35 @@ func (p *PoolWithFuncGeneric[T]) Invoke(arg T) error {
 		p.fn(arg)
 	})
 }
+
+// Running returns running workers
+func (p *PoolWithFuncGeneric[T]) Running() int { return p.pool.Running() }
+
+// Free returns free slots
+func (p *PoolWithFuncGeneric[T]) Free() int { return p.pool.Free() }
+
+// Cap returns capacity
+func (p *PoolWithFuncGeneric[T]) Cap() int { return p.pool.Cap() }
+
+// Submitted returns submitted count
+func (p *PoolWithFuncGeneric[T]) Submitted() uint64 { return p.pool.Submitted() }
+
+// Completed returns completed count
+func (p *PoolWithFuncGeneric[T]) Completed() uint64 { return p.pool.Completed() }
+
+// Tune changes capacity
+func (p *PoolWithFuncGeneric[T]) Tune(size int) { p.pool.Tune(size) }
+
+// Release closes the pool
+func (p *PoolWithFuncGeneric[T]) Release() { p.pool.Release() }
+
+// ReleaseTimeout closes with timeout
+func (p *PoolWithFuncGeneric[T]) ReleaseTimeout(timeout time.Duration) error {
+	return p.pool.ReleaseTimeout(timeout)
+}
+
+// Reboot restarts the pool
+func (p *PoolWithFuncGeneric[T]) Reboot() { p.pool.Reboot() }
+
+// IsClosed checks if closed
+func (p *PoolWithFuncGeneric[T]) IsClosed() bool { return p.pool.IsClosed() }
