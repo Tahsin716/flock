18f6870349b77f08d4640c84bda03ca741593538
Tahsin716 <tahsin.rashad716@gmail.com>
2025-09-30 21:41:14 +0600

    benchmark test added

--- pool/benchmark_test.go
+++ pool/benchmark_test.go
@@ -193,3 +193,129 @@ func BenchmarkDirectGoroutineMemoryAlloc(b *testing.B) {
 	}
 	wg.Wait()
 }
+
+// Realistic benchmarks with actual work
+
+func BenchmarkRealisticCPUWork(b *testing.B) {
+	b.Run("Pool", func(b *testing.B) {
+		pool, _ := NewPool(runtime.GOMAXPROCS(0), WithPreAlloc(true))
+		defer pool.Release()
+
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			pool.Submit(func() {
+				// Simulate CPU work: 1000 iterations
+				sum := 0
+				for j := 0; j < 1000; j++ {
+					sum += j * j
+				}
+				_ = sum
+				wg.Done()
+			})
+		}
+		wg.Wait()
+	})
+
+	b.Run("Goroutines", func(b *testing.B) {
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				sum := 0
+				for j := 0; j < 1000; j++ {
+					sum += j * j
+				}
+				_ = sum
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkRealisticIOWork(b *testing.B) {
+	b.Run("Pool", func(b *testing.B) {
+		pool, _ := NewPool(runtime.GOMAXPROCS(0)*2, WithPreAlloc(true))
+		defer pool.Release()
+
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			pool.Submit(func() {
+				// Simulate I/O wait
+				time.Sleep(100 * time.Microsecond)
+				wg.Done()
+			})
+		}
+		wg.Wait()
+	})
+
+	b.Run("Goroutines", func(b *testing.B) {
+		var wg sync.WaitGroup
+		wg.Add(b.N)
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			go func() {
+				time.Sleep(100 * time.Microsecond)
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+	})
+}
+
+func BenchmarkHighVolume(b *testing.B) {
+	const taskCount = 10000
+
+	b.Run("Pool", func(b *testing.B) {
+		pool, _ := NewPool(100, WithPreAlloc(true))
+		defer pool.Release()
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			var wg sync.WaitGroup
+			wg.Add(taskCount)
+
+			for j := 0; j < taskCount; j++ {
+				pool.Submit(func() {
+					// Small amount of work
+					sum := 0
+					for k := 0; k < 100; k++ {
+						sum += k
+					}
+					_ = sum
+					wg.Done()
+				})
+			}
+			wg.Wait()
+		}
+	})
+
+	b.Run("Goroutines", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			var wg sync.WaitGroup
+			wg.Add(taskCount)
+
+			for j := 0; j < taskCount; j++ {
+				go func() {
+					sum := 0
+					for k := 0; k < 100; k++ {
+						sum += k
+					}
+					_ = sum
+					wg.Done()
+				}()
+			}
+			wg.Wait()
+		}
+	})
+}
