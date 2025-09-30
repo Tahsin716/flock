51c3403412d487f06e4d6faf5dd0d22dd0a38d47
Tahsin716 <tahsin.rashad716@gmail.com>
2025-09-30 20:22:13 +0600

    pool creation test

--- pool/pool_test.go
+++ pool/pool_test.go
@@ -1,1 +1,17 @@
 package pool
+
+import "testing"
+
+// Basic functionality tests
+
+func TestPoolCreation(t *testing.T) {
+	pool, err := NewPool(10)
+	if err != nil {
+		t.Fatalf("Failed to create pool: %v", err)
+	}
+	defer pool.Release()
+
+	if pool.Cap() != 10 {
+		t.Errorf("Expected capacity 10, got %d", pool.Cap())
+	}
+}
